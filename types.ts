2b80f773716598f0c2e0ee4c57ff6cdb2f31da29
nikelborm <evadev@duck.com>
2025-05-07 21:57:20 +0000

    added types.ts

---
+++ types.ts
@@ -0,0 +1,34 @@
+import type { FLAG_ENUM } from './consts.ts';
+
+export type Gen<Context = undefined, Result = never> = Promise<Result> & {
+  ctx: Context;
+} & (
+    | {
+        status: 'pending';
+        isPending: true;
+        isSettled: false;
+        isFulfilled: false;
+        isRejected: false;
+      }
+    | {
+        status: 'fulfilled';
+        isPending: false;
+        isSettled: true;
+        isFulfilled: true;
+        isRejected: false;
+        result: Result;
+      }
+    | {
+        status: 'rejected';
+        isPending: false;
+        isSettled: true;
+        isFulfilled: false;
+        isRejected: true;
+        error: unknown;
+      }
+  );
+
+export type Resolution<Result = never> =
+  | { status: typeof FLAG_ENUM.PENDING; value: undefined }
+  | { status: typeof FLAG_ENUM.FULFILLED; value: Result }
+  | { status: typeof FLAG_ENUM.REJECTED; value: unknown };
