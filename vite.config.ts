5bf625951750b937914be636f4c63e65d693a6bb
nikelborm <evadev@duck.com>
2025-05-08 22:43:31 +0000

    added vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import {
+  defineConfig,
+  coverageConfigDefaults,
+  defaultExclude,
+} from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    exclude: [...defaultExclude, 'tmp/**'],
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reportsDirectory: './gh-page/coverage',
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        'destination/**',
+        'tmp/**',
+        'errors.[jt]s',
+        '**/{scratchpad,index,logObjectPretty}[.][jt]s',
+      ],
+    },
+  },
+});
